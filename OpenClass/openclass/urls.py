e240dfd81af788899752613a9e7ce5c1c82c95d1
youben11 <ayouben9@gmail.com>
2018-03-25 16:09:56 +0100

    setup the urls.py of the openclass app

---
+++ OpenClass/openclass/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "openclass"
+
+urlpatterns = [
+    #path('index/<int:id>/', views.index, kwargs={'this_is_a_model':5555}, name='index'),
+]
