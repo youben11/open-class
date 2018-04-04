53e9a4445e7b572357140f863c02cf74e25e5b29
youben11 <ayouben9@gmail.com>
2018-04-04 18:45:23 +0100

    allowing all hosts

--- OpenClass/OpenClass/settings.py
+++ OpenClass/OpenClass/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5m2!%+3dchj4v(t6!6qo!rtly722aiqs-1iqs6@53-a&w2u@nq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
