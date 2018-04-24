59c647162b8ca90861e9540443dfb55981049c5a
youben11 <ayouben9@gmail.com>
2018-04-24 22:32:50 +0100

    adding models

--- OpenClass/openclass/models.py
+++ OpenClass/openclass/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Workshop(models.Model):
+    pass
+
+class Registration(models.Model):
+    pass
+
+class Question(models.Model):
+    pass
+
+class Feedback(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
+
+class User(models.Model):
+    pass
+
+class Preference(models.Model):
+    pass
+
+class Badge(models.Model):
+    pass
+
+class Have_badge(models.Model):
+    pass
