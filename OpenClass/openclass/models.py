efaf5e2b60b82bb4d470f13710d57ea731839179
youben11 <ayouben9@gmail.com>
2018-04-25 08:10:50 +0100

    defining attributes

--- OpenClass/openclass/models.py
+++ OpenClass/openclass/models.py
@@ -1,28 +1,51 @@
 from django.db import models
 
 class Workshop(models.Model):
-    pass
+    title = #string
+    description = #string
+    material_required = #string
+    what_u_will_learn = #string
+    nb_places = #positiveinteger
+    date = #date
+    location = #string
+    cover_img = #image
+    accepted = #boolean
+
 
 class Registration(models.Model):
-    pass
+    date = #date
+    confirmed = #boolean
 
 class Question(models.Model):
-    pass
+    question = #string
 
 class Feedback(models.Model):
-    pass
+    feedback = #string
 
 class Tag(models.Model):
-    pass
+    name = #string
 
-class User(models.Model):
-    pass
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = #enum
+    score = #positiveinteger
+    phone_number = #string
+    date_birth = #date
+    confirmation_value = #string
+    confirmed = #boolean
+    photo = #image
+    enrollement_date = #date
 
 class Preference(models.Model):
-    pass
+    confidentiality = #integer
 
 class Badge(models.Model):
-    pass
+    name = #string
+    description = #string
+    img = #image
+
+    class Meta:
+        abstract = True
 
 class Have_badge(models.Model):
-    pass
+    priority = #positiveinteger
