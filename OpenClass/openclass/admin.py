8ce1e7a40697be9557d0fe03110491c1e8f48c13
youben11 <ayouben9@gmail.com>
2018-04-25 16:42:59 +0100

    fix coding error

--- OpenClass/openclass/admin.py
+++ OpenClass/openclass/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin, BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
 
