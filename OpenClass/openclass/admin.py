e8209b1f2df8c84a18dc8af0d494c40e5dc685d2
youben11 <ayouben9@gmail.com>
2018-04-25 08:33:55 +0100

    User += Profile in the admin site to be modified

--- OpenClass/openclass/admin.py
+++ OpenClass/openclass/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "profile"
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
