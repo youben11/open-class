055fa06a394a184072c89b04b6538afc629fb061
youben11 <ayouben9@gmail.com>
2018-04-28 23:51:51 +0100

    new testcase: test models and their relationships

---
+++ OpenClass/openclass/tests/tests_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from datetime import datetime, date
+from openclass.models import *
+
+class ModelsTest(TestCase):
+    """Tests all models and their relations with each other"""
+    @classmethod
+    def setUpTestData(cls):
+        cls.badge = Badge.objects.create(name='Best Instructor',
+                                          description='The best instructor',
+                                          img=None)
+        cls.profile = Profile(gender='M', score=100,
+                                phone_number='+21600000',
+                                date_birth=date.today(),
+                                confirmation_value='45abc3',
+                                confirmed=False,
+                                photo=None,
+                                enrollement_date=date.today(),
+                                )
+        cls.user = User.objects.create(username='youben11',
+                                        email='youben@yopmail.com',
+                                        profile=cls.profile)
+        cls.profile.user = cls.user
+        cls.profile.save()
+        cls.workshop = None
+        cls.registration = None
+        cls.question = None
+        cls.feedback = None
+        cls.choice = None
+        cls.mcquestion = None
+        cls.tag = None
+        cls.preference = None
+        cls.have_badge = None
+        cls.badge_attendance = None
+
+    def test_user(self):
+        self.assertEqual(self.user.username, 'youben11')
+        self.assertEqual(self.user.profile, self.profile)
+        self.assertEqual(self.user.profile.confirmation_value, '45abc3')
