a069ff01e8f4e21b1efb6983ffc6c4d8f1a0c4e0
philomath213 <bilalphilomath@gmail.com>
2018-04-29 21:55:55 +0100

    add urls scheme and their corresponding views skeletons

--- OpenClass/openclass/views.py
+++ OpenClass/openclass/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('Hello, Welcome to OpenClass')
+
+def workshops_list(request):
+    return HttpResponse('workshops_list')
+
+def workshops_detail(request, pk):
+    return HttpResponse('workshops_detail')
+
+def members_list(request):
+    return HttpResponse('members_list')
+
+def members_detail(request, pk):
+    return HttpResponse('members_detail')
+
+def badges_list(request):
+    return HttpResponse('badges_list')
