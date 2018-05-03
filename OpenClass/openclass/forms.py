1c7f812511664a1cb67386a234ee5af685f0dcea
youben11 <ayouben9@gmail.com>
2018-05-03 22:35:55 +0100

    basic WorkshopForm

--- OpenClass/openclass/forms.py
+++ OpenClass/openclass/forms.py
@@ -1,6 +1,6 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Workshop
 
 class UserProfileForm(ModelForm):
     class Meta:
@@ -12,3 +12,19 @@ class UserForm(ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+class WorkshopForm(ModelForm):
+
+    class Meta:
+        model = Workshop
+        fields = [
+                'title',
+                'description',
+                'requirements',
+                'objectives',
+                'seats_number',
+                'required_materials',
+                'start_date',
+                'duration',
+                'cover_img',
+                ]
