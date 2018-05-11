a2791353e1d6ac7b5c116f5186e10ce7fae88604
NadirHackerman <nadirmasterrace@gmail.com>
2018-05-11 16:36:41 +0200

    added in comment the sqlite3 database config

--- OpenClass/OpenClassProject/settings.py
+++ OpenClass/OpenClassProject/settings.py
@@ -74,6 +74,13 @@ WSGI_APPLICATION = 'OpenClassProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
