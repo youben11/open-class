59beb20f371f42ef2376954d7d40f0e9c3e16f1e
NadirHackerman <nadirmasterrace@gmail.com>
2018-05-13 21:40:23 +0200

    Switched back the Database configuration to postgresql

--- OpenClass/OpenClassProject/settings.py
+++ OpenClass/OpenClassProject/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'OpenClassProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'openclass',
-#         'USER': 'openclass',
-#         'PASSWORD': 'openclass',
-#         'HOST': 'db',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'openclass',
+        'USER': 'openclass',
+        'PASSWORD': 'openclass',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 
 
 # Password validation
