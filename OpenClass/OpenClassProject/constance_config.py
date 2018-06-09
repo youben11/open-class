31b7ead9f208f0c4ce0e18bd7ebf7281906626c4
NadirHackerman <nadirmasterrace@gmail.com>
2018-06-09 17:01:50 +0200

    Renamed the site title

--- OpenClass/OpenClassProject/constance_config.py
+++ OpenClass/OpenClassProject/constance_config.py
@@ -1,5 +1,5 @@
 CONSTANCE_CONFIG = {
-    'SITE_NAME': ('MyWebSite Title', 'Website title'),
+    'SITE_NAME': ('OpenClass', 'Website title'),
     'SITE_DESCRIPTION': ('', 'Website description'),
     'POINTS_ANIMATION': (20, 'How many points a member gain when animating a workshop'),
     'POINTS_ATTENDANCE': (10, 'How many points a member gain when attending a workshop')
