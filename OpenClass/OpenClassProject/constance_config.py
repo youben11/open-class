a6ab125fb65e86a8bfe37ac8b7d6367e14cb17e2
philomath213 <bilalphilomath@gmail.com>
2018-10-19 17:56:00 +0100

    constance: make all About informations dynamic

--- OpenClass/OpenClassProject/constance_config.py
+++ OpenClass/OpenClassProject/constance_config.py
@@ -1,6 +1,33 @@
+CONSTANCE_ADDITIONAL_FIELDS = {
+    'image_field': ['django.forms.ImageField', {}],
+}
+
+
 CONSTANCE_CONFIG = {
     'SITE_NAME': ('OpenClass', 'Website title'),
-    'SITE_DESCRIPTION': ('', 'Website description'),
+    'SITE_DESCRIPTION': ('OpenClass is a web app that promote information sharing through organized workshops.', 'Website description'),
+    'PROJECT_GITHUB_URL': ('https://github.com/youben11/open-class', 'Project Github URL'),
+
     'POINTS_ANIMATION': (20, 'How many points a member gain when animating a workshop'),
-    'POINTS_ATTENDANCE': (10, 'How many points a member gain when attending a workshop')
+    'POINTS_ATTENDANCE': (10, 'How many points a member gain when attending a workshop'),
+
+    'CORE_TEAM_MEMBER_N0_NAME': ('Nadir Bounoua', 'Core team member N0 name'),
+    'CORE_TEAM_MEMBER_N0_PHOTO': ('team/nadir.png', 'Core team member N0 photo', 'image_field'),
+    'CORE_TEAM_MEMBER_N0_DESCRIPTION': ('Developer', 'Core team member N0 description'),
+
+    'CORE_TEAM_MEMBER_N1_NAME': ('Ayoub Benaissa', 'Core team member N1 name'),
+    'CORE_TEAM_MEMBER_N1_PHOTO': ('team/ayoub.jpg', 'Core team member N1 photo', 'image_field'),
+    'CORE_TEAM_MEMBER_N1_DESCRIPTION': ('Developer', 'Core team member N1 description'),
+
+    'CORE_TEAM_MEMBER_N2_NAME': ('Bilal Retiat', 'Core team member N2 name'),
+    'CORE_TEAM_MEMBER_N2_PHOTO': ('team/bilal.jpg', 'Core team member N2 photo', 'image_field'),
+    'CORE_TEAM_MEMBER_N2_DESCRIPTION': ('Developer', 'Core team member N2 description'),
+
+    'CORE_TEAM_MEMBER_N3_NAME': ('Abdelaziz Attouche', 'Core team member N3 name'),
+    'CORE_TEAM_MEMBER_N3_PHOTO': ('team/aziz.png', 'Core team member N3 photo', 'image_field'),
+    'CORE_TEAM_MEMBER_N3_DESCRIPTION': ('Developer', 'Core team member N3 description'),
+
+    'CORE_TEAM_MEMBER_N4_NAME': ('Ahmed Derkaoui', 'Core team member N4 name'),
+    'CORE_TEAM_MEMBER_N4_PHOTO': ('team/ahmed.jpg', 'Core team member N4 photo', 'image_field'),
+    'CORE_TEAM_MEMBER_N4_DESCRIPTION': ('Developer', 'Core team member N4 description'),
 }
